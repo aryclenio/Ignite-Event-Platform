28c71f8ddde35b5e4b85b01d8eb88be9570f4075
arycleniobarros <arycleniobarros@gmail.com>
2022-06-21 13:45:55 -0300

    ⚡️  Integrating apollo client

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4n4k5w68h3x01yra00r5rzf/master',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4odznws22bk01z24l9p8du1/master',
   cache: new InMemoryCache(),
 })
